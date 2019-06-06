fc33168edd1efd455863c6a19548cc7a8923f9ed
j-brammer <jbrammer@callibrity.com>
2019-06-06 17:22:16 -0400

    added new endpoint to get latest message status

--- src/controllers/encode.controller.ts
+++ src/controllers/encode.controller.ts
@@ -1,6 +1,7 @@
 import { Response, Request, Router, NextFunction } from "express";
 import { Message } from "../models/message.model";
 import * as BitmovinService from "../services/bitmovin.service";
+import * as ContentfulService from "../services/contentful.service";
 
 const router: Router = Router();
 
@@ -17,6 +18,27 @@ router.post('/message', (req: Request, res: Response, next: NextFunction) => {
         })
     })
     .catch(err => res.status(400).send(err))
+});
+
+router.get('/latestMessageStatus', (req: Request, res: Response, next: NextFunction) => {
+  ContentfulService.getLatestMessage()
+    .then(message => {
+      BitmovinService.getEncoding(message.videoId)
+        .then(encoding => {
+          BitmovinService.getManifestForEncoding(encoding.id)
+            .then(manifest => {
+              res.send({
+                messageId: message.id,
+                videoId: message.videoId,
+                encodingStatus: encoding.status,
+                manifestStatus: manifest.status
+              })
+            })
+        })
+    }).catch(error => {
+      res.status(500).send(error);
+      next(error);
+    });
 });
 
 export const EncodeController: Router = router;
