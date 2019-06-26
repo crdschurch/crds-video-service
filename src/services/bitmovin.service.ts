6b52a0369cb043be3e7f0ce55491ec9edfeaa661
Jimmy Janssen <jjanssen@CSMBP0010-JJ.local>
2019-06-26 13:20:39 -0400

    remove subtitles in manifest

--- src/services/bitmovin.service.ts
+++ src/services/bitmovin.service.ts
@@ -48,7 +48,6 @@ async function startEncoding(contentData: ContentData) {
   };
 
   const manifest = await bitmovin.encoding.manifests.hls.create(manifestConfig);
-  await addSubtitles(manifest, contentData);
   await Promise.all(await createAudioManifest(audioMuxingConfigs, encoding, manifest));
   await Promise.all(await createVideoManifest(videoMuxingConfigs, encoding, manifest));
   await bitmovin.encoding.manifests.hls(manifest.id).start();
@@ -213,7 +212,6 @@ async function createVideoManifest(videoMuxingConfigs, encoding, manifest) {
     .map(videoMuxingConfig => {
       var variantStream = {
         audio: 'audio_group',
-        subtitles: 'subtitles_group',
         closedCaptions: 'NONE',
         segmentPath: 'video/' + videoMuxingConfig.streams[0].streamId + '/',
         uri: `video${videoMuxingConfig.streams[0].streamId}.m3u8`,
@@ -274,28 +272,3 @@ const waitUntilHlsManifestFinished = manifest => {
     waitForManifestToBeFinished();
   });
 };
-
-const createHlsVttMedia = (hlsManifest, vttUrl) => {
-  return new Promise((resolve, reject) => {
-    const vttMedia = {
-      name: 'en',
-      groupId: 'subtitles_group',
-      language: 'en',
-      vttUrl,
-      uri: 'vtt_media.m3u8'
-    };
-
-    bitmovin.encoding.manifests
-      .hls(hlsManifest.id)
-      .media.vtt.add(vttMedia)
-      .then(createdVttMedia => {
-        console.log('Successfully created HLS VTT Media', createdVttMedia);
-        resolve(createdVttMedia);
-      })
-  });
-};
-
-async function addSubtitles(manifest, contentfulRecord) {
-  if (!contentfulRecord.transcriptionUrl) return;
-  return createHlsVttMedia(manifest, `https:${contentfulRecord.transcriptionUrl}`);
-}
