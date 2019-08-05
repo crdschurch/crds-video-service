49f3554f6f5d8cde604244ead94c952a29c73340
j-brammer <jbrammer@callibrity.com>
2019-08-05 10:35:34 -0400

    added new method to set metadata for front end downloads of mp4

--- src/services/s3.service.ts
+++ src/services/s3.service.ts
@@ -1,5 +1,6 @@
 import { S3 } from 'aws-sdk';
 import { ContentData } from '../models/contentful-data.model';
+import { mp4VideoCodecs } from './bitmovin.codec';
 
 var s3 = new S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -19,10 +20,31 @@ export function hasDownloads(contentfulData: ContentData) {
       return true;
     })
     .catch(err => {
-      if(err.code === 'NoSuchKey'){
+      if (err.code === 'NoSuchKey') {
         return false;
       } else {
         throw new Error(err);
       }
+    })
+}
+
+export function setMetaDataForMp4(contentfulData: ContentData): Promise<any>[] {
+  return mp4VideoCodecs
+    .map(codec => {
+      const params = {
+        Bucket: `${process.env.BITMOVIN_BUCKET}/${process.env.BITMOVIN_DIRECTORY}`,
+        CopySource: `${process.env.BITMOVIN_BUCKET}/${process.env.BITMOVIN_DIRECTORY}/${contentfulData.videoId}/${contentfulData.title}_${codec.type}.mp4`,
+        Key: `${contentfulData.videoId}/${contentfulData.title}_${codec.type}.mp4`,
+        ContentDisposition: 'attachment',
+        ContentType: 'application/mp4',
+        MetadataDirective: 'REPLACE',
+        ACL: 'public-read'
+      };
+
+      return s3.copyObject(params)
+        .promise()
+        .then(data => {
+          console.log(`Metadata properly set for ${contentfulData.title}_${codec.type}.mp4`);
+        })
     })
 }
